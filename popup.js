2a154b3cf7e5eb96a335b90e942fcb347dbfc45e
NickGroesch <nicholasgroesch2019@u.northwestern.edu>
2021-03-14 14:27:16 -0500

    looping over all the css properties from a file in triple nesting

--- popup.js
+++ popup.js
@@ -17,24 +17,79 @@ changeColor.addEventListener("click", async () => {
 
 // The body of this function will be execuetd as a content script inside the
 // current page
-function setPageBackgroundColor() {
+async function setPageBackgroundColor() {
   chrome.storage.sync.get("color", ({ color }) => {
     document.body.style.backgroundColor = color;
   });
+  const theCssRules = []
+
   const linkNodes = document.querySelectorAll('link[rel=stylesheet]')
-  linkNodes.forEach(node => {
+  linkNodes.forEach(async node => {
     console.log(node.getAttribute("href"))
     const nodesHref = node.getAttribute("href")
+
     if (nodesHref.slice(0, 5) == "https") {
       //probably cdn
       console.log("not gonna do it")
     } else {
       //probably local css
-      fetch(nodesHref)
+      const css = await fetch(nodesHref)
         .then(res => res.text())
-        .then(css => console.log(css))
+      const cssLines = css.split("\n")
+      console.log(cssLines)
+      //this approach only works for politely formatted source css
+
+      //parse minified or bundled css differently
+      //from  https://stackoverflow.com/questions/3326494/parsing-css-in-javascript-jquery
+      //You can easily use the Browser's own CSSOM to parse CSS:
+
+      var extractRulesForCssText = function (styleContent) {
+
+        var doc = document.implementation.createHTMLDocument(""),
+          styleElement = document.createElement("style");
+
+        styleElement.textContent = styleContent;
+        // the style will only be parsed once it is added to a document
+        doc.body.appendChild(styleElement);
+
+        return styleElement.sheet.cssRules;
+      };
+      const cssRules = (extractRulesForCssText(css))
+      theCssRules.push(cssRules)
+      // const cssRulesLen = cssRules.length
+      // console.log(cssRules)
+      // console.log(cssRulesLen)
+
+      // for (let index = 0; index < cssRulesLen; index++) {
+      //   const rule = cssRules[index];
+      //   console.log(rule.styleMap)
+      //   console.log(typeof rule.style)
+      //   console.log(Object.keys(rule.style))
+      // }
     }
+
   })
 
+  setTimeout(() => analyzeRules(), 3000)
+  const analyzeRules = () => {
+    theCssRules.forEach(ruleSet => {
+      console.log(ruleSet.length)
+      for (let index = 0; index < ruleSet.length; index++) {
+        const rule = ruleSet[index];
+        console.log(rule?.href)//import rules will have an href
+        //other style rules
+        console.log(rule)
+        console.log(rule.style)
+        if (rule?.style?.length) {
+          for (let index = 0; index < rule?.style?.length; index++) {
+            const ruleKey = rule.style[index];
+            console.log(ruleKey)
+            console.log(rule.style[ruleKey])
+          }
+        }
+
+      }
+    })
+  }
 
 }
